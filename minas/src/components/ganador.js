766df98b9d86eabaca79e66f52b76eae2354ecdf
jorge <jherpue553@g.educaan.es>
2023-12-05 10:17:12 +0100

    Guardo una cosa que me faltó

--- minas/src/components/ganador.js
+++ minas/src/components/ganador.js
@@ -2,12 +2,10 @@ import './ganador.css';
 
 function Ganador(props) {
     const mensaje = props.ganado ? "¡Has ganado!":"¡Has perdido!";
-    const ganado = props.ganado;
 
     return (
         <div className="ganador" >
-            {ganado && <h1 color='red'>{mensaje}</h1>}
-            {!ganado && <h1 color='green'>{mensaje}</h1>}
+            <h1>{mensaje}</h1>
         </div>
     );
 }
