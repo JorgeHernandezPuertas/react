273953bb95884729cd90b10463a024da949f063f
jorge <jherpue553@g.educaan.es>
2024-01-17 12:21:23 +0100

    Pongo los datos de los pueblos en un archivo aparte

--- buscador/src/App.js
+++ buscador/src/App.js
@@ -2,11 +2,9 @@ import './App.css'
 import { useState } from 'react'
 import Buscador from './components/Buscador.js'
 import Resultado from './components/Resultado.js'
+import { pueblos } from './pueblos.js'
 
 function App () {
-  const [pueblos] = useState([
-    'Estepona', 'Sabinillas', 'San pedro', 'Cancelada', 'Ojen', 'Malaga', 'Marbella', 'Fuengirola', 'Los Barrios', 'Mijas', 'Fuenlabrada', 'Motril'
-  ])
   const [busqueda, setBusqueda] = useState('')
 
   // Establezco la busqueda cada vez que se cambia el buscador
---
+++ buscador/src/pueblos.js
@@ -0,0 +1,3 @@
+export const pueblos = [
+  'Estepona', 'Sabinillas', 'San pedro', 'Cancelada', 'Ojen', 'Malaga', 'Marbella', 'Fuengirola', 'Los Barrios', 'Mijas', 'Fuenlabrada', 'Motril'
+]
