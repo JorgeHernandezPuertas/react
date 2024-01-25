7f866f272cf474935c07fcf979acd7658d99d2ae
JorgeHernandezPuertas <jherpue553@g.educaand.es>
2024-01-25 19:37:01 +0100

    Cambio y mejoro el ejercicio de la modal

--- prueba-modal-class/src/App.js
+++ prueba-modal-class/src/App.js
@@ -10,59 +10,56 @@ const VentanaModalDiccionario = (props) => {
   } = props;
 
   const [elegido, setElegido] = useState()
+  const [options] = useState(['CODIGO1|DESCRIPCION1', 'CODIGO2|DESCRIPCION2', 'CODIGO3|DESCRIPCION3', 'CODIGO4|DESCRIPCION4', 'CODIGO5|DESCRIPCION5'])
 
   const handleChange = (event) => { // Buscador de filtro
     // COMPLETA ESTA FUNCION
     let elegidoAux;
-    // Recupero el select
-    const select = document.getElementById('selectMulti')
-    // Hago un array con todos sus hijos
-    const elementos = Array.from(select.children)
     // Me quedo con los hijos en los que matcheen la busqueda y mapeo el array para quedarme con los valores
-    const matchings = elementos.filter((e) => e.innerHTML.toLowerCase().includes(event.target.value)).map((e) => e.innerHTML)
+    const matchings = options.filter((e) => e.toLowerCase().includes(event.target.value.toLowerCase()))
     // Si tengo algun match pongo el primero como value
     if (matchings[0] !== undefined) {
-      select.value = matchings[0]
       // Pongo la elegida por ahora donde tiene que ir
       elegidoAux = matchings[0]
     } else {
-      elegidoAux = elementos[0].innerHTML
+      elegidoAux = ""
     }
     setElegido(elegidoAux)
   }
 
+  const handleClick = (event) => {
+    setElegido(event.target.value)
+  }
+
   return (
     <div>
-      <Modal isOpen={props.mostrar} toggle={props.toggle} className={className} onEntering={"//ESTO SE EJECUTA CUANDO MUESTRAS LA VENTANA"}>
-        <ModalHeader toggle={props.toggle}>{props.titulo}</ModalHeader>
+      <Modal isOpen={ props.mostrar } toggle={ props.toggle } className={ className } onEntering={ "//ESTO SE EJECUTA CUANDO MUESTRAS LA VENTANA" }>
+        <ModalHeader toggle={ props.toggle }>{ props.titulo }</ModalHeader>
         <ModalBody>
           <FormGroup row>
-            <Label sm={2} > Filtrar: </Label>
-            <Col sm={10}>
-              <Input onChange={handleChange}
+            <Label sm={ 2 } > Filtrar: </Label>
+            <Col sm={ 10 }>
+              <Input onChange={ handleChange }
                 id="filtro"
                 name="filtro"
                 type="Text" />
             </Col>
           </FormGroup>
           <FormGroup row>
-            <Col sm={12}>
-              <Input onChange={handleChange} onClick={handleChange}
+            <Col sm={ 12 }>
+              <Input onChange={ handleChange } onClick={ handleClick }
                 id="selectMulti"
                 name="selectMulti"
                 type="select"
+                value={ elegido }
               >
-                <option>CODIGO1|DESCRIPCION1</option>
-                <option>CODIGO2|DESCRIPCION2</option>
-                <option>CODIGO3|DESCRIPCION3</option>
-                <option>CODIGO4|DESCRIPCION4</option>
-                <option>CODIGO5|DESCRIPCION5</option>
+                { options.map((e) => <option>{ e }</option>) }
               </Input>
             </Col>
           </FormGroup>
         </ModalBody>
         <ModalFooter>
-          {elegido}<Button color="primary" onClick={() => props.add(document.getElementById('selectMulti').value)}>{props.aceptar}</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          { elegido }<Button color="primary" onClick={ () => props.add(elegido) }>{ props.aceptar }</Button>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
         </ModalFooter>
       </Modal>
     </div>
@@ -76,42 +73,48 @@ class Filter extends Component {
     super(props);
     this.state = {
       isOpen: false,
-      tipo: "agregar"
+      tipo: "agregar",
+      medicinasIncluidas: [],
+      medicinasExcluidas: [],
     }
   }
 
-  toggleModal(tipoAux) {
+  toggleModal (tipoAux) {
     this.setState({ isOpen: !this.state.isOpen, tipo: tipoAux })
   }
 
-  add(datos) {
+  add (datos) {
     //aqui hacer algo con los datos
     // Recupero el textarea dependiendo de si es tipo agregar o quitar
-    let textarea
-    if (this.state.tipo === "agregar"){
-      textarea = document.getElementById('rxseleccionar')
-    } else {
-      textarea = document.getElementById('rxenmascarar')
-    }
-    textarea.innerHTML += datos + "\n"
-    this.toggleModal();
-  }
-
-  clear(tipo) {
-    if (tipo === "agregar"){
-      document.getElementById('rxseleccionar').innerHTML = ''
-    } else {
-      document.getElementById('rxenmascarar').innerHTML = ''
+    if (datos !== undefined) {
+      if (this.state.tipo === "agregar") {
+        const medicinaAux = this.state.medicinasIncluidas.slice()
+        medicinaAux.push(datos)
+        this.setState({ medicinasIncluidas: medicinaAux })
+      } else {
+        const medicinaAux = this.state.medicinasExcluidas.slice()
+        medicinaAux.push(datos)
+        this.setState({ medicinasExcluidas: medicinaAux })
+      }
+      this.toggleModal();
     }
   }
 
-  render() {
+  clear (tipo) {
+    if (tipo === "agregar") {
+      this.setState({ medicinasIncluidas: [] })
+    } else {
+      this.setState({ medicinasExcluidas: [] })
+    }
+  }
+
+  render () {
     return <>
       <div>
         <UncontrolledAccordion
-          defaultOpen={[
+          defaultOpen={ [
             '1'
-          ]}
+          ] }
           stayOpen
         >
           <AccordionItem>
@@ -123,17 +126,17 @@ class Filter extends Component {
                 <Col>
                   <Alert color="info">
                     Incluir X Medicamentos:
-                    <Input type="textarea" name="rxseleccionar" id='rxseleccionar' />
-                    <Button color="info" onClick={()=>{this.toggleModal("agregar")}}>Add</Button>
-                    {" "}<Button color="info" onClick={() => (this.clear("agregar"))}>Clear</Button>
+                    <Input type="textarea" name="rxseleccionar" id='rxseleccionar' value={ this.state.medicinasIncluidas.map((e1) => e1 + "\n") } />
+                    <Button color="info" onClick={ () => this.toggleModal("agregar") }>Add</Button>
+                    { " " }<Button color="info" onClick={ () => (this.clear("agregar")) }>Clear</Button>
                   </Alert>
                 </Col>
                 <Col>
                   <Alert color="danger">
                     Excluir X Medicamentos:
-                    <Input type="textarea" name="rxenmascarar" id='rxenmascarar' />
-                    <Button color="danger" onClick={()=>{this.toggleModal("quitar")}}>Add</Button>
-                    {" "}<Button color="danger" onClick={() => (this.clear("quitar"))}>Clear</Button>
+                    <Input type="textarea" name="rxenmascarar" id='rxenmascarar' value={ this.state.medicinasExcluidas.map((e) => e + '\n') } />
+                    <Button color="danger" onClick={ () => { this.toggleModal("quitar") } }>Add</Button>
+                    { " " }<Button color="danger" onClick={ () => (this.clear("quitar")) }>Clear</Button>
                   </Alert>
                 </Col>
               </Row>
@@ -141,11 +144,11 @@ class Filter extends Component {
           </AccordionItem>
         </UncontrolledAccordion>
       </div>
-      <VentanaModalDiccionario 
-        add={(datos) => this.add(datos)} 
-        mostrar={this.state.isOpen} 
-        aceptar={"Añadir"}
-        titulo={"VENTANA MODAL"}
+      <VentanaModalDiccionario
+        add={ (datos) => this.add(datos) }
+        mostrar={ this.state.isOpen }
+        aceptar={ "Añadir" }
+        titulo={ "VENTANA MODAL" }
       />
       <br />
     </>
@@ -153,7 +156,7 @@ class Filter extends Component {
 }
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <Filter />
