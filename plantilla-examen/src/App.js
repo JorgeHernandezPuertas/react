2443554ae7e1a1828ebbebf24934c781b2144f23
jorge <jherpue553@g.educaand.es>
2024-02-01 12:34:07 +0100

    Creo la plantilla para el examen

---
+++ plantilla-examen/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import { Button } from 'reactstrap'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button >Prueba</Button>
+    </div>
+  );
+}
+
+export default App;
