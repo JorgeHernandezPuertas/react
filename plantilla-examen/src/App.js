04bdb2266e29b90ec72ef37d7e3d11c45a40b91d
jorge <jherpue553@g.educaand.es>
2024-02-02 10:10:59 +0100

    Dejo la plantilla del examen para el siguiente

--- plantilla-examen/src/App.js
+++ plantilla-examen/src/App.js
@@ -1,13 +1,25 @@
-import './App.css';
-import { Button } from 'reactstrap'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React, { Component, useState } from "react";
+import { Button, Input, Modal, ModalHeader, ModalBody, ModalFooter, FormGroup, Label, Col } from 'reactstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {
-  return (
-    <div className="App">
-      <Button >Prueba</Button>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      // INSERTE AQUÍ EL ESTADO NECESARIO. AQUÍ SE GUARDARÁ TODA LA INFORMACIÓN
+      // CADA TELÉFONO TIENE QUE SER ÚNICO
+      listaUsuarios: [],
+      isOpen: false,
+      informacion: "",
+    };
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Button>Holi</Button>
+      </div>
+    );
+  }
 }
-
 export default App;
