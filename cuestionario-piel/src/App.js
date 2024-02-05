2d3fca53fd42964a290c993c6a9f35f1e89d5fcb
jorge <jherpue553@g.educaand.es>
2024-02-05 14:35:50 +0100

    Avanzo

--- cuestionario-piel/src/App.js
+++ cuestionario-piel/src/App.js
@@ -1,16 +1,12 @@
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
 
-import { TIPOS_PIEL, PREGUNTAS, VALORES } from './datos/datos.js'
+import { PREGUNTAS } from './datos/datos.js'
 import { Button } from 'reactstrap'
 import { useState } from 'react'
 import Test from './componentes/Test.js'
 
 function App () {
-  console.log(TIPOS_PIEL)
-  console.log(PREGUNTAS)
-  console.log(VALORES)
-
   const [empezado, setEmpezado] = useState(false)
   const [pregunta, setPregunta] = useState(0)
   const [respuestas, setRespuestas] = useState(Array(PREGUNTAS.length))
@@ -38,7 +34,7 @@ function App () {
     <div className="App">
       <h1>Descubre tu fototipo</h1>
       { !empezado && <Button className='btn-start' color='primary' onClick={ start } >Empezar test</Button> }
-      { empezado && <Test preguntaActual={ pregunta } next={ next } previous={ previous } seleccionar={seleccionar} /> }
+      { empezado && <Test preguntaActual={ pregunta } next={ next } previous={ previous } seleccionar={seleccionar} respuestas={respuestas} /> }
     </div>
   )
 }
--- cuestionario-piel/src/componentes/Test.js
+++ cuestionario-piel/src/componentes/Test.js
@@ -1,23 +1,33 @@
 import { Button } from 'reactstrap'
 import { PREGUNTAS } from '../datos/datos.js'
 
-export default function Test ({ preguntaActual, next, previous, seleccionar }) {
-  const nombreDiff = crypto.randomUUID()
-  const respuestasActuales = PREGUNTAS[preguntaActual].respuestas.map((r, i) => {
+export default function Test ({ preguntaActual, next, previous, seleccionar, respuestas }) {
+  const respuestasActuales = <div className='respuestas' key={crypto.randomUUID()} >{PREGUNTAS[preguntaActual].respuestas.map((r, i) => {
     const newId = crypto.randomUUID()
-    return (<div>
-      <input type='radio' id={newId} name={ nombreDiff } onClick={() => seleccionar(i)} /> <label for={newId}>{ r.texto }</label>
-    </div>)
-  })
+    const opcion = respuestas[preguntaActual] === i
+      ? <div>
+    <label for={newId}><input key={newId} defaultChecked type='radio' id={newId} onClick={() => seleccionar(i)} /> { r.texto }</label>
+  </div>
+      : <div>
+    <label for={newId}><input key={newId} type='radio' id={newId} onClick={() => seleccionar(i)} /> { r.texto }</label>
+  </div>
+    return opcion
+  })}</div>
+
+  let mostrar
+
+  if (preguntaActual === 0) {
+    mostrar = <>{respuestasActuales}<Button onClick={next} >{'>'}</Button></>
+  } else if (preguntaActual === PREGUNTAS.length - 1) {
+    mostrar = <><Button onClick={previous} >{'<'}</Button>{respuestasActuales}</>
+  } else {
+    mostrar = <><Button onClick={previous} >{'<'}</Button>{respuestasActuales}<Button onClick={next} >{'>'}</Button></>
+  }
 
   return (
     <div className="test">
       <h2>{ PREGUNTAS[preguntaActual].pregunta }</h2>
-      <Button onClick={previous} >{'<'}</Button>
-      <div className='respuestas' >
-        { respuestasActuales }
-      </div>
-      <Button onClick={next} >{'>'}</Button>
+        { mostrar }
     </div>
   )
 }
