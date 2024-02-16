505f4ac27f363dd76e8ce4f36cfdb2e20d18e43e
jorge <jherpue553@g.educaand.es>
2024-02-16 12:53:14 +0100

    Avanzo el supermarket

--- supermarkets/src/App.js
+++ supermarkets/src/App.js
@@ -1,9 +1,31 @@
 import './App.css'
+import { useState } from 'react'
+import { POBLACION2 } from './datos/datos.js'
 
 function App () {
+  const [poblacion, setPoblacion] = useState(POBLACION2)
+
+  // Mostrar población
+  const mostrarPoblacion = <div className='tabla'>{poblacion.map((fila, y) => {
+    const nuevaFila = <div key={crypto.randomUUID()} className='fila'>{fila.map((columna, x) => {
+      const supermarket = poblacion[y][x].supermarket.abierto ? 'supermarket' : ''
+      const nuevaCol = <button key={crypto.randomUUID()} className={`${supermarket} col`} onClick={nuevoMarket}>{columna.personas}</button>
+      return nuevaCol
+    })}</div>
+    return nuevaFila
+  })}</div>
+
+  function nuevoMarket () {
+    const aux = JSON.parse(JSON.stringify(poblacion))
+    aux[2][2].supermarket = { abierto: true, nombre: 'carrefour', clientes: 0 }
+    setPoblacion(aux)
+  }
+
   return (
     <div className="App">
-      <p>Prueba</p>
+      <h2>Población</h2>
+      {mostrarPoblacion}
+      <button onClick={nuevoMarket}>Añadir</button>
     </div>
   )
 }
