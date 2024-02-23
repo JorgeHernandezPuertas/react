e1621e400d461b87b6a34e1029077362ed134f0b
jorge <jherpue553@g.educaand.es>
2024-02-23 10:53:29 +0100

    Termino el supermarkets

--- supermarkets/src/componentes/Supermercados.js
+++ supermarkets/src/componentes/Supermercados.js
@@ -12,14 +12,18 @@ export default function Supermercados ({ poblacion }) {
     return fila
   })
 
-  const mostrarSupers = <ul>
-    {
-      supers.map((s, index) => {
-        const clientes = Math.ceil(s.clientes * 100) / 100
-        return <li key={crypto.randomUUID()}>{index + 1}. &emsp;{`El supermercado con posición (${s.posicion.x + 1}, ${s.posicion.y + 1}) tiene ${clientes} clientes`}</li>
-      })
-    }
-  </ul>
+  let mostrarSupers = <p>No hay supermercados en este momento</p>
+
+  if (supers.length !== 0) {
+    mostrarSupers = <ul>
+      {
+        supers.map((s, index) => {
+          const clientes = Math.ceil(s.clientes * 100) / 100
+          return <li key={crypto.randomUUID()}>{index + 1}. &emsp;{`El supermercado con posición (${s.posicion.x + 1}, ${s.posicion.y + 1}) tiene ${clientes} clientes`}</li>
+        })
+      }
+    </ul>
+  }
 
   return (
     <div className="supermercados">
