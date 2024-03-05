a362e6be6b17adf1a52ace77210a5e8e5a7cfe5b
jorge <jherpue553@g.educaand.es>
2024-03-05 12:40:19 +0100

    Empiezo

--- precio-pisos/src/App.js
+++ precio-pisos/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { useState, useEffect } from 'react'
+import { create, all } from 'mathjs'
+import Formulario from './components/Formulario'
 
-function App() {
+function App () {
+  const [vector, setVector] = useState()
+  const [math] = useState(create(all, {}))
+  const [datosUsuario] = useState([{ metros: 0 }, { habitaciones: 0 }, { baños: 0 }, { vista: 0 }, { garaje: 0 }, { trastero: 0 }, { año: 0 }, { estado: 0 }, { piscina: 0 }])
+  const [precio] = useState(0)
+
+  console.log(vector, datosUsuario, precio)
+
+  // Calculo el vector de regresión multivariable
+  useEffect(() => {
+    setVector(math.matrix([1, 2, 3]))
+  }, [math])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Formulario datosUsuario={datosUsuario} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ precio-pisos/src/components/Formulario.js
@@ -0,0 +1,13 @@
+import { Button } from 'reactstrap'
+
+export default function Formulario ({ datosUsuario }) {
+  const campos = datosUsuario.map(() => {
+
+  })
+
+  return (
+    <div className="formulario">
+
+    </div>
+  )
+}
