d1f1c79dc0a9361188b7524a7326fbe7cacf2420
jorge <jherpue553@g.educaand.es>
2024-03-06 12:45:36 +0100

    Avanzo

--- precio-pisos/src/App.js
+++ precio-pisos/src/App.js
@@ -3,23 +3,54 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import { useState, useEffect } from 'react'
 import { create, all } from 'mathjs'
 import Formulario from './components/Formulario'
+import { PISOS, PRECIOS } from './datos/datos.js'
 
 function App () {
   const [vector, setVector] = useState()
   const [math] = useState(create(all, {}))
-  const [datosUsuario] = useState([{ metros: 0 }, { habitaciones: 0 }, { baños: 0 }, { vista: 0 }, { garaje: 0 }, { trastero: 0 }, { año: 0 }, { estado: 0 }, { piscina: 0 }])
-  const [precio] = useState(0)
+  const [datosUsuario, setDatosUsuario] = useState([])
+  const [precio, setPrecio] = useState()
 
   console.log(vector, datosUsuario, precio)
 
   // Calculo el vector de regresión multivariable
   useEffect(() => {
-    setVector(math.matrix([1, 2, 3]))
+    // Convierto mis datos en matrices que pueda tratar
+    const MATRIZ_PISOS = math.matrix(PISOS)
+    const MATRIZ_PRECIOS = math.matrix(PRECIOS)
+
+    // Aplico la fórmula de la normal para calcular el vector de regresión lineal
+    const VECTOR = math.multiply(math.multiply(math.inv(math.multiply(math.transpose(MATRIZ_PISOS), MATRIZ_PISOS)), math.transpose(MATRIZ_PISOS)), MATRIZ_PRECIOS)
+    console.log('Vector final:')
+    console.log(VECTOR)
+    setVector(VECTOR)
   }, [math])
+
+  useEffect(() => {
+    if (vector) {
+      let resultado = 0
+      vector.map((valor, indice) => {
+        resultado += valor * datosUsuario[indice]
+        return valor
+      })
+      setPrecio(resultado)
+    }
+  }, [datosUsuario, vector])
+
+  function establecerDatos (datos) {
+    setDatosUsuario(datos)
+  }
 
   return (
     <div className="App">
-      <Formulario datosUsuario={datosUsuario} />
+      <Formulario establecerDatos={establecerDatos} />
+      {
+        precio &&
+        <div>
+          <h4>Precio</h4>
+          <p>El precio de tu piso es {precio}</p>
+        </div>
+      }
     </div>
   )
 }
--- precio-pisos/src/components/Formulario.js
+++ precio-pisos/src/components/Formulario.js
@@ -1,12 +1,123 @@
-import { Button } from 'reactstrap'
+import { Button, Form, FormGroup, Input, Label } from 'reactstrap'
 
-export default function Formulario ({ datosUsuario }) {
-  const campos = datosUsuario.map(() => {
+export default function Formulario ({ establecerDatos }) {
+  const ANIO_MAX = (new Date()).getFullYear()
 
-  })
+  const enviar = (e) => {
+    e.preventDefault()
+    const form = e.target
+    const datos = [form.metros.value, form.habitaciones.value, form.banios.value, form.vistas.value, form.garaje.value, form.trastero.value, form.anio.value, form.estado.value, form.piscina.value]
+    establecerDatos(datos)
+  }
 
   return (
     <div className="formulario">
+      <h2>Datos de la vivienda</h2>
+      <Form onSubmit={enviar}>
+        <FormGroup>
+          <Label for='metros'>
+            Metros
+          </Label>
+          <Input
+            id='metros'
+            name='metros'
+            placeholder='Metros de la vivienda...'
+            type="number"
+            min={1}
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='habitaciones'>
+            Habitaciones
+          </Label>
+          <Input
+            id='habitaciones'
+            name='habitaciones'
+            placeholder='Habitaciones de la vivienda...'
+            type="number"
+            min={1}
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='banios'>
+            Baños
+          </Label>
+          <Input
+            id='banios'
+            name='banios'
+            placeholder='Baños de la vivienda...'
+            type="number"
+            min={1}
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='vistas'>
+            ¿Tiene vistas al mar?
+          </Label>
+          <Input
+            id='vistas'
+            name='vistas'
+            type="checkbox"
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='garaje'>
+            ¿Tiene garaje?
+          </Label>
+          <Input
+            id='garaje'
+            name='garaje'
+            type="checkbox"
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='trastero'>
+            ¿Tiene trastero?
+          </Label>
+          <Input
+            id='trastero'
+            name='trastero'
+            type="checkbox"
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='anio'>
+            Año de construcción
+          </Label>
+          <Input
+            id='anio'
+            name='anio'
+            placeholder='Año de la vivienda...'
+            type="number"
+            max={ANIO_MAX}
+            min={1800}
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='estado'>
+            Estado actual de la vivienda (del 1 al 5)
+          </Label>
+          <Input
+            id='estado'
+            name='estado'
+            placeholder='Estado de la vivienda...'
+            type="number"
+            min={1}
+            max={5}
+          />
+        </FormGroup >
+        <FormGroup>
+          <Label for='piscina'>
+            ¿Tiene piscina?
+          </Label>
+          <Input
+            id='piscina'
+            name='piscina'
+            type="checkbox"
+          />
+        </FormGroup >
+        <Button color='primary' >Calcular</Button>
+      </Form>
 
     </div>
   )
